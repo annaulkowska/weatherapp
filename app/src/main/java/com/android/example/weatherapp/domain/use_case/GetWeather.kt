14dd1536f7854081948d3472b14ebbfaad983a39
Anna Ulkowska <annaulkowska89@gmail.com>
2022-03-07 22:30:31 +0100

    Add unit test for use case

--- app/src/main/java/com/android/example/weatherapp/domain/use_case/GetWeather.kt
+++ app/src/main/java/com/android/example/weatherapp/domain/use_case/GetWeather.kt
@@ -6,15 +6,11 @@ import com.android.example.weatherapp.domain.model.WeatherInfo
 import com.android.example.weatherapp.domain.repository.WeatherRepository
 import com.plcoding.dictionary.core.util.Resource
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class GetWeather @Inject constructor( private val repository: WeatherRepository) {
 
     operator fun invoke(gps: Gps): Flow<Resource<WeatherInfo>> {
-        if(gps.lat == -1.0 || gps.lon == -1.0) {
-            return flow {  }
-        }
         return repository.getWeatherInfo(gps.lat, gps.lon, WeatherUtils.EXCLUDE, WeatherUtils.UNITS, WeatherUtils.API_ID)
     }
 }
--- app/src/test/java/com/android/example/weatherapp/domain/use_case/GetWeatherTest.kt
+++ app/src/test/java/com/android/example/weatherapp/domain/use_case/GetWeatherTest.kt
@@ -1,5 +1,56 @@
 package com.android.example.weatherapp.domain.use_case
 
-import org.junit.Assert.*
+import com.android.example.weatherapp.core.util.WeatherUtils
+import com.android.example.weatherapp.domain.model.WeatherInfo
+import com.android.example.weatherapp.domain.repository.WeatherRepository
+import com.google.common.truth.Truth.assertThat
+import com.plcoding.dictionary.core.util.Resource
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Test
 
-class GetWeatherTest
+class GetWeatherTest {
+
+    var repository: WeatherRepository = mockk()
+    lateinit var tested: GetWeather
+    var weatherInfoResource: Resource<WeatherInfo> = mockk()
+
+    @Before
+    fun setUp() {
+        tested = GetWeather(repository)
+    }
+
+    @Test
+    fun `WHEN called with gps location (lat, lon) THEN weather info is returned`() {
+        val testGPS = WeatherUtils.MAINZ
+        every {
+            repository.getWeatherInfo(
+                testGPS.lat,
+                testGPS.lon,
+                any(),
+                any(),
+                any()
+            )
+        } returns flowOf(weatherInfoResource)
+
+        val result = tested.invoke(testGPS)
+
+        verify {
+            repository.getWeatherInfo(
+                testGPS.lat,
+                testGPS.lon,
+                WeatherUtils.EXCLUDE,
+                WeatherUtils.UNITS,
+                WeatherUtils.API_ID
+            )
+        }
+        runBlocking {
+            assertThat(result.first()).isEqualTo(weatherInfoResource)
+        }
+    }
+}
